b88a08ca79128e832e647c80982bc57e8d56ca1a
felipeSouza <felipefavasouza@gmail.com>
2022-11-14 21:24:21 -0300

    pushing react docs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Components/Title/Title'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title title='Hola'></Title>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Title/Title.jsx
@@ -0,0 +1,6 @@
+
+export default function Title({title}){
+    return(
+        <h1>{title}</h1>
+    )
+}
